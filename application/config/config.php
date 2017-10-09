9b9550645c432b196ebdcf5e4e99bdeaed0d5139
Franco Bornancini <franconbornancini@gmail.com>
2017-10-09 10:53:29 -0300

    Update

--- application/config/config.php
+++ application/config/config.php
@@ -23,9 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://www.fivepixelstudio.com';
+$config['base_url'] = '';
 
-/*
+/*http://www.fivepixelstudio.com
 |--------------------------------------------------------------------------
 | Index File
 |--------------------------------------------------------------------------
--- application/views/inicio.php
+++ application/views/inicio.php
@@ -1,63 +1,48 @@
-<div class="container">
-	<div class="row">
-		<div class="col-sm-12">
-			<h2>GAME PANEL</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum maxime tempore quae eum provident, ipsa nihil quidem veritatis consequatur quod molestias veniam labore quos odio doloremque modi iusto necessitatibus. Provident.</p>
-			<?php echo base_url(); ?>
-		</div>
-	</div>
-</div>
+  <!--Banner starts here-->
+    <section id="banner">
 
-<br>
-<div class="container">
-  <div class="row">
- 
-    <!-- GAME  -->   
-    <div class="col-sm-4 game">
-      <div class="panel panel-default">
-        <img src="http://www.abc.net.au/reslib/201612/r1646745_25272999.jpg" class="img-responsive" alt="Image">
-        <div class="panel-body">
-          <h4>LIMBO</h4>
-          <p class="descripcion">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus facere harum magnam ducimus sit earum voluptatibus voluptate consectetur! Eum dolorum animi sunt, esse deserunt nisi assumenda cum obcaecati debitis sint?</p>
-          <div class="text-center"><button type="button" class="btn btn-default"><i class="fa fa-gamepad" aria-hidden="true"></i> Jugar </button></div>
+        <div class="row">
+            <div class="col-md-12">
+                <div id="bg-slider" class="carousel slide" data-ride="carousel">
+
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner" role="listbox">
+                        <div class="item active one">
+                            <div class="carousel-caption">
+                                <h2>Lorem Ipsum is simply dummy text of the printing</h2>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                    when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap 
+                                    into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+                                    and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                            </div>
+
+                        </div>
+                        <div class="item two">
+                            <div class="carousel-caption">
+                                <h2>Lorem Ipsum is simply dummy text of the printing 2</h2>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                    when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap 
+                                    into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+                                    and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                            </div>
+
+                        </div>
+                        <div class="item three">
+                            <div class="carousel-caption">
+                                <h2>Lorem Ipsum is simply dummy text of the printing 3</h2>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                    when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap 
+                                    into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+                                    and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    
+                </div>
+
+            </div>
         </div>
-      </div>
-    </div> 
 
-    <!-- GAME  -->
-    <div class="col-sm-4 game">
-      <div class="panel panel-default">
-        <img src="http://www.abc.net.au/reslib/201612/r1646745_25272999.jpg" class="img-responsive" alt="Image">
-        <div class="panel-body">
-          <h4>LIMBO</h4>
-          <p class="descripcion">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus facere harum magnam ducimus sit earum voluptatibus voluptate consectetur! Eum dolorum animi sunt, esse deserunt nisi assumenda cum obcaecati debitis sint?</p>
-          <div class ="text-center"><button type="button" class="btn btn-default"><i class="fa fa-gamepad" aria-hidden="true"></i> Jugar</button></div>
-        </div>
-      </div>
-    </div>
-
-    <!-- GAME  -->
-    <div class="col-sm-4 game">
-      <div class="panel panel-default">
-        <img src="http://www.abc.net.au/reslib/201612/r1646745_25272999.jpg" class="img-responsive" alt="Image">
-        <div class="panel-body">
-          <h4>LIMBO</h4>
-          <p class="descripcion">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus facere harum magnam ducimus sit earum voluptatibus voluptate consectetur! Eum dolorum animi sunt, esse deserunt nisi assumenda cum obcaecati debitis sint?</p>
-          <div class="text-center"><button type="button" class="btn btn-default"><i class="fa fa-gamepad" aria-hidden="true"></i> Jugar</button></div>
-        </div>
-      </div>
-    </div>
-
-  </div>
-</div>
-
-<?php if($this->session->userdata('log_estado') == 'OK'){ ?>
-<div class="container">
-	<div class="row">
-		<div class="col-sm-12">
-			<h2>DATO SOLO PARA USUARIOS</h2>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero ullam laudantium, ipsum repellat ex quam deserunt temporibus porro laborum quidem veniam voluptas voluptate harum maiores est ratione, esse debitis nulla?</p>
-		</div>
-	</div>
-</div>
-<?php } ?>
+    </section>
